6a8752c79840c397b6b97109b77ad257f3570b42
Giovanna <giovannad1clonius@gmail.com>
2023-07-01 22:39:15 -0300

    get address from cep completed

--- src/services/enrollments-service/index.ts
+++ src/services/enrollments-service/index.ts
@@ -5,18 +5,22 @@ import addressRepository, { CreateAddressParams } from '@/repositories/address-r
 import enrollmentRepository, { CreateEnrollmentParams } from '@/repositories/enrollment-repository';
 import { exclude } from '@/utils/prisma-utils';
 
-// TODO - Receber o CEP por parâmetro nesta função.
-async function getAddressFromCEP() {
+async function getAddressFromCEP(cep: string) {
 
-  // FIXME: está com CEP fixo!
-  const result = await request.get(`${process.env.VIA_CEP_API}/37440000/json/`);
+  const result = await request.get(`${process.env.VIA_CEP_API}/${cep}/json/`);
 
   if (!result.data) {
     throw notFoundError();
   }
 
-  // FIXME: não estamos interessados em todos os campos
-  return result.data;
+  return {
+    logradouro: result.data.logradouro,
+    complemento:  result.data.complemento,
+    bairro:  result.data.bairro,
+    cidade:  result.data.localidade,
+    uf:  result.data.uf,
+    erro: result.data.erro
+  };
 }
 
 async function getOneWithAddressByUserId(userId: number): Promise<GetOneWithAddressByUserIdResult> {
